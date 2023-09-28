ab4e0691a0c57894b31b518cb19128cd3a1093a8
Emiliano Spada <emilianos13@gmail.com>
2023-09-28 23:05:30 +0200

    first commit

---
+++ aws-sg-dynamic-ip-update/lambda_function.py
@@ -0,0 +1,55 @@
+import boto3
+import socket
+
+# AWS clients
+ec2 = boto3.client('ec2')
+
+# Security Group and FQDN configuration
+security_group_id = 'YOUR_SECURITY_GROUP_ID'
+fqdn = 'example.com'
+
+def lambda_handler(event, context):
+    try:
+        # Fetch the public IP address for the FQDN
+        ip_address = get_public_ip_from_fqdn(fqdn)
+
+        # Update the security group inbound rule
+        update_security_group(security_group_id, ip_address)
+
+        return {
+            'statusCode': 200,
+            'body': f'Security group rule updated with IP: {ip_address}'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}'
+        }
+
+def get_public_ip_from_fqdn(fqdn):
+    try:
+        ip_address = socket.gethostbyname(fqdn)
+        return ip_address
+    except socket.gaierror:
+        raise Exception(f'Could not resolve IP address for FQDN: {fqdn}')
+
+def update_security_group(security_group_id, ip_address):
+    try:
+        ec2.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpPermissions=[
+                {
+                    'IpProtocol': 'tcp',
+                    'FromPort': 80,  # Port you want to update
+                    'ToPort': 80,
+                    'IpRanges': [
+                        {
+                            'CidrIp': f'{ip_address}/32',  # /32 denotes a single IP address
+                            'Description': 'Updated via Lambda'
+                        },
+                    ],
+                },
+            ],
+        )
+    except Exception as e:
+        raise Exception(f'Error updating security group rule: {str(e)}')
